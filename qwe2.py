e8f54850e762a4a5b2e64351c7c81eccd42225b9
lgblkb <dbakhtiyarov@nu.edu.kz>
2020-08-22 03:44:39 +0600

    Developing deployment logic

--- qwe2.py
+++ qwe2.py
@@ -4,13 +4,17 @@ import itertools as it
 import more_itertools as mit
 
 
+
 def main():
-    aeval = Interpreter(usersyms=dict(string=string, it=it, mit=mit))
-    aeval.symtable['qwe'] = """
-qweqweqwe
-    """
-    res = aeval('qwe.strip()')
-    print(res)
+    #     aeval = Interpreter(usersyms=dict(string=string, it=it, mit=mit))
+    #     aeval.symtable['qwe'] = """
+    # qweqweqwe
+    #     """
+    #     res = aeval('qwe.strip()')
+    #     print(res)
+    filepath = r'/home/lgblkb/PycharmProjects/proj_1/provision/library/eval.py'
+
+
 
     pass
 
