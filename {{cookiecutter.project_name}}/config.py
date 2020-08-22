3defef0bf7bca6120da4459e39a8b62172e55076
lgblkb <dbakhtiyarov@nu.edu.kz>
2020-08-23 01:05:46 +0600

    Abandon cookiecutter!

--- {{cookiecutter.project_name}}/config.py
+++
@@ -1,9 +0,0 @@
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix="{{cookiecutter.project_name}}".capitalize(),
-    settings_files=['settings.yaml', '.secrets.yaml'],
-)
-
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load this files in the order.
