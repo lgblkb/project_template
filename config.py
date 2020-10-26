8cbb3c9216bf87700ea7101bf02dff42fcddafea
lgblkb <dbakhtiyarov@nu.edu.kz>
2020-10-26 18:25:08 +0600

    Updated provision

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="{{cookiecutter.project_name}}".capitalize(),
     settings_files=['settings.yaml', '.secrets.yaml'],
+    environments=True,
+    load_dotenv=True,
+
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
