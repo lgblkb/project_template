dcf5ef10179da6d7cf72785b4d8b55b69ad7fc5b
lgblkb <dbakhtiyarov@nu.edu.kz>
2021-01-11 15:58:01 +0600

    Major refactoring done to provision

---
+++ app/__main__.py
@@ -0,0 +1,18 @@
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('qwe')
+
+
+def main():
+    logger.info('Start')
+    for i in range(100):
+        time.sleep(1)
+        logger.info("i: %s", i)
+    logger.info('Finish')
+    pass
+
+
+if __name__ == '__main__':
+    main()
