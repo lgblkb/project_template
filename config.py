3f1aee83fb1bec87e86ad144d62e22b2b0f13402
lgblkb <dbakhtiyarov@nu.edu.kz>
2021-11-06 17:18:39 +0600

    Update

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
 from box import Box
 
 settings = Box.from_yaml(filename='settings.yaml').settings
+
+cache_folder = Path(settings.PROJECT.dirs.cache_folder)
+data_folder = Path(settings.PROJECT.dirs.data_folder)
