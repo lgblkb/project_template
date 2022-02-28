c7232279e613343ee3384cb0f0e426af2717f59d
lgblkb <dbakhtiyarov@nu.edu.kz>
2022-02-28 11:47:07 +0600

    Update

--- provision/commands/docker_build.sh
+++
@@ -1,6 +0,0 @@
-docker image build -t test_project_template-development:latest \
-	--target builder \
-	--build-arg USER_ID=1000 \
-	--build-arg GROUP_ID=1000 \
-	--build-arg USERNAME=lgblkb \
-	--build-arg PROJECT_DIR=/home/lgblkb/PycharmProjects/project_template /home/lgblkb/PycharmProjects/project_template "$@"
