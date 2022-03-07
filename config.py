20531d7d859eed6f4165913d6c35a38f3574109e
lgblkb <dbakhtiyarov@nu.edu.kz>
2022-03-07 21:39:23 +0600

    Update

--- config.py
+++ config.py
@@ -4,5 +4,12 @@ from box import Box
 
 settings = Box.from_yaml(filename='settings.yaml').settings
 
-cache_folder = Path(settings.PROJECT.dirs.cache_folder)
-data_folder = Path(settings.PROJECT.dirs.data_folder)
+
+def get_folder(path: str):
+    folder = Path(path).joinpath(settings.PROJECT.name)
+    folder.mkdir(exist_ok=True)
+    return folder
+
+
+cache_folder = get_folder(settings.PROJECT.dirs.cache_folder)
+data_folder = get_folder(settings.PROJECT.dirs.data_folder)
